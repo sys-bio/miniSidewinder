cfcd01ba0510b83257fb0ef8a08ef7a01c743583
bartjuw <barthj@uw.edu>
2023-03-02 13:05:16 -0800

    initial checkin for webpage template for libretexts and other websites that want to incorporate minisidewinder

---
+++ docs/libretexts/miniSidewinderSimSetup.js
@@ -0,0 +1,42 @@
+
+ 
+ 
+ function getInfo(iframeId, modelSBML, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel) {
+   if (typeof window !== 'undefined') {
+ // Perform sessionStorage action
+   const item = sessionStorage.getItem('key')
+   }
+   console.log('3. iframeId: ', iframeId);
+   sessionStorage.setItem("MODEL", modelSBML);
+   sessionStorage.setItem('RUNTIME', newRT);
+   sessionStorage.setItem('STEPSIZE', newSS);
+   sessionStorage.setItem('STATIC', staticRun);
+   sessionStorage.setItem('SLIDERS', parSliders);
+   sessionStorage.setItem('PLOT_SPECIES', spPlot);
+   sessionStorage.setItem('X_LABEL', xLabel);
+   sessionStorage.setItem('Y_LABEL', yLabel);
+  // window.location.href = ('Debug/index.html?')
+   document.getElementById(iframeId).src = 'https://bartjuw.github.io/miniSidewinder/Debug/index.html'; // Location of miniSidewinder
+ }
+ 
+
+function runModel(frameId, modelURL, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel) {
+// Use this code if you have a predefined model to run located at some URL:
+//fetch('http://localhost/models/simpleRXN.xml')
+//fetch('models/BIOMD0098_Goldbeter1990.xml')
+fetch(modelURL)
+  .then(response => response.text())
+  .then((data) => {
+    //console.log(data);
+	newModel = data;
+	getInfo(frameId, newModel, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel);
+  })
+  
+ }
+
+
+
+
+
+  
+  
