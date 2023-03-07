a96a60e641b5a9ed8e7a847294c1aae8140d7556
bartjuw <barthj@uw.edu>
2023-03-07 09:56:50 -0800

    template and helper functions for libreTexts author to load model into miniSidewinder

--- docs/libretexts/miniSidewinderSimSetup.js
+++ docs/libretexts/miniSidewinderSimSetup.js
@@ -1,13 +1,13 @@
 
- 
+// Functions which grab user model to run and then call the simulator (minSidewinder). 
  
  function getInfo(iframeId, modelSBML, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel) {
    if (typeof window !== 'undefined') {
  // Perform sessionStorage action
    const item = sessionStorage.getItem('key')
    }
-   console.log('3. iframeId: ', iframeId);
-   sessionStorage.setItem("MODEL", modelSBML);
+   console.log('getInfo: iframeId: ', iframeId);
+   sessionStorage.setItem("MODEL", modelSBML); // model: XML formated string 
    sessionStorage.setItem('RUNTIME', newRT);
    sessionStorage.setItem('STEPSIZE', newSS);
    sessionStorage.setItem('STATIC', staticRun);
@@ -16,18 +16,20 @@
    sessionStorage.setItem('X_LABEL', xLabel);
    sessionStorage.setItem('Y_LABEL', yLabel);
   // window.location.href = ('Debug/index.html?')
-   document.getElementById(iframeId).src = 'https://bartjuw.github.io/miniSidewinder/Debug/index.html'; // Location of miniSidewinder
+   document.getElementById(iframeId).src = '/@api/deki/files/69599/miniSidewinder_index.html'; // Location of miniSidewinder
  }
  
 
 function runModel(frameId, modelURL, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel) {
+  console.log('miniSidewinderSimSetup.runModel(): Values Recieved:');
+  console.log(frameId, modelURL, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel);
 // Use this code if you have a predefined model to run located at some URL:
 //fetch('http://localhost/models/simpleRXN.xml')
 //fetch('models/BIOMD0098_Goldbeter1990.xml')
 fetch(modelURL)
   .then(response => response.text())
   .then((data) => {
-    //console.log(data);
+    console.log(data);
 	newModel = data;
 	getInfo(frameId, newModel, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel);
   })
