becad28525745cd0406c56f0e789c03a7581be94
bartjuw <barthj@uw.edu>
2023-03-09 12:15:22 -0800

    add debug statements and try catch around fetch statement

--- docs/libretexts/miniSidewinderSimSetup.js
+++ docs/libretexts/miniSidewinderSimSetup.js
@@ -15,6 +15,9 @@
    sessionStorage.setItem('PLOT_SPECIES', spPlot);
    sessionStorage.setItem('X_LABEL', xLabel);
    sessionStorage.setItem('Y_LABEL', yLabel);
+   // TEST:
+   const params = sessionStorage.getItem('SLIDERS');
+   console.log('getInfo() - sessionStorage(SLIDERS):', params);
   // window.location.href = ('Debug/index.html?')
    //window.location.href = ('/@api/deki/files/69599/miniSidewinder_index.html');
   document.getElementById(iframeId).src = '/@api/deki/files/69599/miniSidewinder_index.html'; // Location of miniSidewinder
@@ -22,18 +25,23 @@
  
 
 function runModel(frameId, modelURL, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel) {
-  console.log('miniSidewinderSimSetup.runModel(): Values Recieved:');
+  console.log('miniSidewinderSimSetup.runModel(): Values Received:');
   console.log(frameId, modelURL, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel);
 // Use this code if you have a predefined model to run located at some URL:
 //fetch('http://localhost/models/simpleRXN.xml')
 //fetch('models/BIOMD0098_Goldbeter1990.xml')
-fetch(modelURL)
-  .then(response => response.text())
-  .then((data) => {
-    console.log(data);
-	newModel = data;
-	getInfo(frameId, newModel, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel);
-  })
+ try {
+   fetch(modelURL)
+     .then(response => response.text())
+     .then((data) => {
+       console.log(data);
+	   newModel = data;
+	   getInfo(frameId, newModel, newRT, newSS, staticRun, parSliders, spPlot, xLabel, yLabel);
+     })
+ } catch(error) {
+	 var errmsg = 'Model not loaded from: ' + modelURL + '. ' + error;
+	 alert(errmsg);
+   }
   
  }
 
